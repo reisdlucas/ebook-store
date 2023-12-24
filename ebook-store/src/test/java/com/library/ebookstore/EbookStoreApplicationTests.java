f62ca54069d37d84586e297bd128a688415a80a0
Lucas Reis <reisdlucas@icloud.com>
2023-12-24 02:47:29 -0300

    :sparkles: First commit: create project

---
+++ ebook-store/src/test/java/com/library/ebookstore/EbookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.ebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
