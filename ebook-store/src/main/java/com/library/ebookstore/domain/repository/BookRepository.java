858701727ab4c60e09e81dc0befccc6bb547633a
Lucas Reis <reisdlucas@icloud.com>
2023-12-24 03:18:42 -0300

    :hammer: (REPOSITORY): create book repository

---
+++ ebook-store/src/main/java/com/library/ebookstore/domain/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.library.ebookstore.domain.repository;
+
+import com.library.ebookstore.domain.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
