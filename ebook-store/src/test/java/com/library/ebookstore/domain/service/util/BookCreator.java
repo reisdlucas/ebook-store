ff14eadd3a277647a40b43d2850e151bf5f6004d
reisdlucas <reisdlucas@icloud.com>
2023-12-29 12:36:45 -0300

    :white_check_mark: (TEST): Create bookRequest and bookResponse builder

--- ebook-store/src/test/java/com/library/ebookstore/domain/service/util/BookCreator.java
+++ ebook-store/src/test/java/com/library/ebookstore/domain/service/util/BookCreator.java
@@ -1,5 +1,7 @@
 package com.library.ebookstore.domain.service.util;
 
+import com.library.ebookstore.api.http.resources.request.BookRequest;
+import com.library.ebookstore.api.http.resources.response.BookResponse;
 import com.library.ebookstore.domain.model.Book;
 
 public class BookCreator {
@@ -9,6 +11,23 @@ public class BookCreator {
                 .name("name")
                 .writer("writer")
                 .company("company")
+                .build();
+    }
+
+    public static BookRequest bookRequest() {
+        return BookRequest.builder()
+                .name(newBook().getName())
+                .writer(newBook().getWriter())
+                .company(newBook().getCompany())
+                .build();
+    }
+
+    public static BookResponse bookResponse() {
+        return BookResponse.builder()
+                .id(newBook().getId())
+                .name(newBook().getName())
+                .writer(newBook().getWriter())
+                .company(newBook().getCompany())
                 .build();
     }
 }
