1a39c114fd514ff46f63accfbe81281541ef2f8c
Lucas Reis <reisdlucas@icloud.com>
2023-12-30 21:56:28 -0300

    :sparkles: (CONTROLLER): Add findAll method

--- ebook-store/src/main/java/com/library/ebookstore/api/controller/BookController.java
+++ ebook-store/src/main/java/com/library/ebookstore/api/controller/BookController.java
@@ -6,6 +6,7 @@ import com.library.ebookstore.core.mapper.BookMapper;
 import com.library.ebookstore.domain.model.Book;
 import com.library.ebookstore.domain.service.BookService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/book")
@@ -44,7 +47,7 @@ public class BookController {
     public ResponseEntity<BookResponse> createBook(@RequestBody @Valid BookRequest bookRequest,
                                                    UriComponentsBuilder uriComponentsBuilder) {
         Book book = bookMapper.mapRequestToEntity(bookRequest);
-        Book bookSaved = bookService.createBook(book);
+        Book bookSaved = bookService.create(book);
 
         URI uri = uriComponentsBuilder.path("/book/{id}")
                 .buildAndExpand(bookSaved.getId())
@@ -52,4 +55,19 @@ public class BookController {
         BookResponse bookResponse = bookMapper.mapEntityToResponse(bookSaved);
         return ResponseEntity.created(uri).body(bookResponse);
     }
+    @GetMapping
+    @ResponseBody
+    @Operation(description = "Find All Books", summary = "Find All Books")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfull Operation",
+            content = @Content(array = @ArraySchema(schema = @Schema(implementation = BookResponse.class)))),
+    })
+    public ResponseEntity<List<BookResponse>> findAllBooks() {
+        List<Book> book = bookService.findAll();
+        List<BookResponse> bookResponse = book.stream()
+                .map(bookMapper::mapEntityToResponse)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(bookResponse);
+    }
+
 }
