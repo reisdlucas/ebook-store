a781973fe540322cff43517ff1e091415eaf6c2b
reisdlucas <reisdlucas@icloud.com>
2024-06-27 14:13:57 -0300

    desabilita strict

--- target/generated-sources/annotations/com/library/ebookstore/core/mapper/BookMapperImpl.java
+++ target/generated-sources/annotations/com/library/ebookstore/core/mapper/BookMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-01-09T16:36:20-0300",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Amazon.com Inc.)"
+    date = "2024-06-11T13:38:05-0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Amazon.com Inc.)"
 )
 @Component
 public class BookMapperImpl implements BookMapper {
